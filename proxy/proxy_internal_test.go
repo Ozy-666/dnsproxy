c9d9863317b8eb78af2a417d34c989163f391dee
Eugene <e.burkov@adguard.com>
2026-07-02 20:54:04 +0500

    all: add formerr responses (4)
    (cherry picked from commit 0d3732b31b7259acf32bdf8b15c24eff616c24e7)

--- proxy/proxy_internal_test.go
+++ proxy/proxy_internal_test.go
@@ -1440,7 +1440,7 @@ func TestProxy_validateRequest(t *testing.T) {
 			Question: []dns.Question{},
 		},
 		addr:            testAddr,
-		wantRcode:       dns.RcodeServerFailure,
+		wantRcode:       dns.RcodeFormatError,
 		wantNil:         false,
 		isPrivateClient: false,
 	}, {
